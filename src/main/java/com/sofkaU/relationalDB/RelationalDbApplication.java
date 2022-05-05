ed49dc593f2d88ff6f895f29db0824257e7b866e
lsepu <55538804+lsepu@users.noreply.github.com>
2022-05-05 13:56:21 -0500

    Initialize backend repository

---
+++ src/main/java/com/sofkaU/relationalDB/RelationalDbApplication.java
@@ -0,0 +1,13 @@
+package com.sofkaU.relationalDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationalDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelationalDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofkaU/relationalDB/RelationalDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkaU.relationalDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationalDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
