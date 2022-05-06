8c793d929ffa515741d725c7a8a04b8eabb08a6d
lsepu <55538804+lsepu@users.noreply.github.com>
2022-05-06 15:48:12 -0500

    REFACTOR: Remove unuseful comments and add better identation

--- src/main/java/com/sofkaU/relationalDB/service/CategoryService.java
+++ src/main/java/com/sofkaU/relationalDB/service/CategoryService.java
@@ -21,13 +21,11 @@ public class CategoryService implements ICategoryService {
     @Autowired
     private NoteRepository noteRepository;
 
-    //Mapper used for the DTO
     @Autowired
     private ModelMapper modelMapper;
 
     @Override
     public List<CategoryDTO> findAllCategories() {
-        //map all categories to the DTO
         List<Category> categories = categoryRepository.findAll();
         List<CategoryDTO> categoriesDTO = categories
                 .stream()
@@ -38,26 +36,19 @@ public class CategoryService implements ICategoryService {
 
     @Override
     public CategoryDTO createCategory(CategoryDTO categoryDTO) {
-
         Category category = modelMapper.map(categoryDTO, Category.class);
-        //set empty list instead of null for notes
         category.setNotes(Collections.emptyList());
-        //save in repository
         category = categoryRepository.save(category);
-
-        //returns a DTO
         categoryDTO = modelMapper.map(category, CategoryDTO.class);
         return categoryDTO;
     }
 
     @Override
     public boolean deleteCategory(Long categoryId) {
-        //check if category with that id exists
         boolean exists = categoryRepository.existsById(categoryId);
         if(!exists){
             return false;
         }
-        //remove all notes related to that category
         Category categoryToBeDeleted = categoryRepository.findById(categoryId).get();
         if (categoryToBeDeleted.getNotes().size() >= 0) {
             categoryToBeDeleted.getNotes().forEach(note -> noteRepository.deleteById(note.getId()));
@@ -65,6 +56,5 @@ public class CategoryService implements ICategoryService {
 
         categoryRepository.deleteById(categoryId);
         return true;
-
     }
 }
--- src/main/java/com/sofkaU/relationalDB/service/NoteService.java
+++ src/main/java/com/sofkaU/relationalDB/service/NoteService.java
@@ -18,42 +18,34 @@ public class NoteService implements INoteService {
     @Autowired
     private NoteRepository noteRepository;
 
-    //Mapper used for the DTO
     @Autowired
     private ModelMapper modelMapper;
 
     @Override
     public NoteDTO createNote(NoteDTO noteDTO) {
-
         Note note = modelMapper.map(noteDTO, Note.class);
-        //set done initially as false
         note.setDone(false);
         Category category = categoryRepository.findById(note.getFkCategoryId()).get();
         category.addNote(note);
-        //save in repository
         noteRepository.save(note);
         categoryRepository.save(category);
-        //return Note DTO
+
         noteDTO = modelMapper.map(note , NoteDTO.class);
         return noteDTO;
-
     }
 
     @Override
     public boolean deleteNote(Long noteId) {
-        //check if exists
         boolean exists = noteRepository.existsById(noteId);
         if(!exists){
             return false;
         }
         noteRepository.deleteById(noteId);
         return true;
-
     }
 
     @Override
     public NoteDTO updateNote(NoteDTO noteDTO) {
-        //check if exists
         boolean exists = noteRepository.existsById(noteDTO.getId());
         if(!exists){
             throw new IllegalStateException(
